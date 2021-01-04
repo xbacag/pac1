68748585dab336a7a9057ebd506e8ff9c3d534d1
Xavier Baca <xbaca@CROSSWIND-HP.localdomain>
2021-01-04 20:54:23 +0100

    pac3 commit

--- dist/PAC1.e31bb0bc.js
+++ dist/PAC1.e31bb0bc.js
@@ -160,7 +160,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62254" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62626" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
